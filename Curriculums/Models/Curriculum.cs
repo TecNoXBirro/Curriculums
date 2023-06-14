3ccaeaa13c32644f16785f829f58a27efc3f9ff0
TecNo <TecNo@DESKTOP-V71MDET>
2023-06-14 15:09:14 +0200

    Agregar archivos de proyecto.

---
+++ Curriculums/Models/Curriculum.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Curriculums.Models
+{
+    public class Curriculum {
+        public string? Tittle { get; set;}
+        [JsonPropertyName("img")]
+        public string? Image { get; set; }
+
+        public override string ToString()
+        {
+            JsonSerializer.Serialize<Curriculum>(this);
+        }
+    }
+}
